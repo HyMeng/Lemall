9eebe8f48446f5b491bc8dc498380cde8719274c
HyMeng <13937680019@163.com>
2018-07-12 22:28:20 +0800

    管理-首页功能完成

--- public/admin/js/common.js
+++ public/admin/js/common.js
@@ -12,6 +12,34 @@ $(document).ajaxStart(function(){
     NProgress.start();
 })
 $(document).ajaxStop(function(){
-    // 关闭进度条
-    NProgress.done();
+    setTimeout(function() {
+        // 关闭进度条
+        NProgress.done();
+    },500)
+})
+
+//二级菜单显示与隐藏效果
+$(".subnav").prev().on("click", function (e) {
+    $(this).next().slideToggle();
+  });
+//侧边栏隐藏
+$('.icon_menu').on('click',function(){
+    $('body').toggleClass('active');
+    $('.le_aside').toggleClass('active');
+    $('.header').toggleClass('active');
+})
+// 退出登录功能
+$(".icon_logout").on('click',function() {
+    $('#myModal').modal('show');
+})
+$('.btn_logout').on('click',function(){
+    $.ajax({
+        url: '/employee/employeeLogout',
+        success: function(info) {
+            // console.log(info);
+            if(info.success){
+                location.href = './login.html';
+            }
+        }
+    })
 })
